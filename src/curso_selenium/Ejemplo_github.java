883e1d0c55c224a35db8b3cf507bee1caa9a6d4b
Brayan <brayan.tabares@ucp.edu.co>
2024-04-03 11:39:36 -0500

    Update Ejemplo_github.java
    Class update.

--- src/curso_selenium/Ejemplo_github.java
+++ src/curso_selenium/Ejemplo_github.java
@@ -4,7 +4,7 @@ public class Ejemplo_github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Esta es una modificación al archivo de ejemplo github
 	}
 
 }
