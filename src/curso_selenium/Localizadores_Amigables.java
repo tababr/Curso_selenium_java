cd0ec13578ea0df046d1ddf5919bc2b884b91f6c
Brayan <brayan.tabares@ucp.edu.co>
2024-04-06 11:14:41 -0500

    Create Localizadores_Amigables.java
    Added relative locators.

---
+++ src/curso_selenium/Localizadores_Amigables.java
@@ -0,0 +1,32 @@
+package curso_selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+
+public class Localizadores_Amigables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.Edge.driver", "C:\\Users\\User\\OneDrive - sport enlace sas\\Documents\\Drivers\\edgedriver\\edgedriver.exe");
+		WebDriver driver_firefox = new FirefoxDriver();
+		driver_firefox.get("https://www.google.com");
+		
+		//Encontrando elemento ubicado a la izquierda de otro elemento
+		//driver_firefox.findElement(RelativeLocator.with(By.tagName("a")).toLeftOf(By.className("gb_h"))).click();
+		
+		//Encontrando elemento ubicado a la derecha de otro elemento
+		//driver_firefox.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.linkText("Gmail"))).click();
+		
+		//Encontrando elemento ubicado
+		driver_firefox.findElement(RelativeLocator.with(By.tagName("a")).toLeftOf(By.className("gb_h")).toLeftOf(By.linkText("Gmail"))).click();
+		
+		driver_firefox.close();
+
+	}
+
+}
